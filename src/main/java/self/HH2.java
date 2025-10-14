bd830f22244aea5bc20a40c775fb7a7c458107cf
hj <hyedii_9@naver.com>
2025-10-14 18:31:50 +0900

    251014-1

---
+++ src/main/java/self/HH2.java
@@ -0,0 +1,11 @@
+package self;
+
+public class HH2 {
+    public static void main(String[] args) {
+        String url="jdbc:oracle:thin:@//localhost:1521/XEPDB1";
+        String user="scott";
+        String pw="tiger";
+
+
+    }
+}
